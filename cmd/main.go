ab1fc27997175562302da456f6fae5346fe653cc
Jaewook Oh <kyle.oh95@gmail.com>
2021-01-28 12:21:30 +0900

    2021-01-28 main 업데이트

--- cmd/main.go
+++ cmd/main.go
@@ -52,33 +52,12 @@ func main() {
 		LabelSelector: label,
 		FieldSelector: field,
 	}
-	pvcs, err := api.PersistentVolumeClaims(ns).List(context.TODO(), listOptions)
-	fmt.Println(len(pvcs.Items))
-
-	// for {
-	// 	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-	// 	if err != nil {
-	// 		panic(err.Error())
-	// 	}
-	// 	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-	// Examples for error handling:
-	// - Use helper functions like e.g. errors.IsNotFound()
-	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-	// namespace := "default"
-	// pod := "example-xxxxx"
-	// _, err = clientset.CoreV1().Pods(namespace).Get(pod, metav1.GetOptions{})
-	// if errors.IsNotFound(err) {
-	// 	fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-	// } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-	// 	fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-	// 		pod, namespace, statusError.ErrStatus.Message)
-	// } else if err != nil {
-	// 	panic(err.Error())
-	// } else {
-	// 	fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-	// }
-
-	// 	time.Sleep(10 * time.Second)
-	// }
+	svcs, err := api.Services(ns).List(context.TODO(), listOptions)
+	fmt.Println(len(svcs.Items))
+	for _, svc := range svcs.Items {
+		if svc.Spec.Type == "LoadBalancer" {
+			ingress := svc.Status.LoadBalancer.Ingress
+			fmt.Println(svc.Name, svc.Namespace, svc.Spec.Type, ingress)
+		}
+	}
 }
